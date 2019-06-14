0c83a353adc96d807d55898254286a497f758b3b
AnneshaMondal <42523989+AnneshaMondal@users.noreply.github.com>
2019-06-14 13:25:49 -0500

    code added to VirtualMachineIIS.ps1

---
+++ VirtualMachine/VirtualMachineIIS.ps1
@@ -0,0 +1,35 @@
+#creates a virtual machine and uses the Azure Custom Script extension to install IIS
+#after running the script you can access the default IIS website on the public IP address of the virtual machine
+
+# Variables for common values
+$resourceGroup = "myResourceGroup"
+$location = "westeurope"
+$vmName = "myVM"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Create a virtual machine
+New-AzVM `
+  -ResourceGroupName $resourceGroup `
+  -Name $vmName `
+  -Location $location `
+  -ImageName "Win2016Datacenter" `
+  -VirtualNetworkName "myVnet" `
+  -SubnetName "mySubnet" `
+  -SecurityGroupName "myNetworkSecurityGroup" `
+  -PublicIpAddressName "myPublicIp" `
+  -Credential $cred `
+  -OpenPorts 80
+
+# Install IIS
+$PublicSettings = '{"commandToExecute":"powershell Add-WindowsFeature Web-Server"}'
+
+Set-AzVMExtension -ExtensionName "IIS" -ResourceGroupName $resourceGroup -VMName $vmName `
+  -Publisher "Microsoft.Compute" -ExtensionType "CustomScriptExtension" -TypeHandlerVersion 1.4 `
+  -SettingString $PublicSettings -Location $location
+
+  
